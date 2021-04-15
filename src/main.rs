ceaa232715011834aaa85af01b061cdab8d89835
Kobby Pentangeli <kobbypentangeli@gmail.com>
2021-04-15 09:11:31 +0000

    feat: Complete the application

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,8 @@
 
 use itertools::Itertools;
 use serde::Deserialize;
-use std::fs::File;
+use std::fs;
 use std::time::{Duration, SystemTime};
-use walkdir::WalkDir;
 
 /// Structure of a block of JSON data
 #[derive(Clone, Debug, Deserialize, PartialEq)]
@@ -18,23 +17,12 @@ pub struct Message {
 }
 
 fn main() -> std::io::Result<()> {
-    // Extract files
-    let mut files: Vec<File> = Vec::new();
-    for entry in WalkDir::new("../stream").into_iter().filter_map(|e| e.ok()) {
-        let json_file_path = entry.path();
-        let file = File::open(json_file_path).expect("File not found");
-        files.push(file);
-    }
-
-    // Map files to `Message` structs
-    let mut packets: Vec<Vec<Message>> = Vec::new();
-    for file in files {
+    for entry in fs::read_dir("./stream")? {
+        let dir = entry?;
+        let path = dir.path();
+        let file = fs::File::open(path).expect("File not found");
         let data: Vec<Message> = serde_json::from_reader(file).expect("Error while reading");
-        packets.push(data);
-    }
 
-    // Iterate over the list of files
-    for data in &packets {
         // Remove duplicates
         let uniques = data
             .clone()
@@ -44,8 +32,8 @@ fn main() -> std::io::Result<()> {
 
         // Calculate metrics
         let good_lines = uniques.len();
-        let maximum_gap = &packets.len() - uniques.len();
-        let average_gap = packets.len() / maximum_gap;
+        let maximum_gap = &data.len() - uniques.len();
+        let average_gap = data.len() / maximum_gap;
 
         println!(
             "-----------------------------------------------------------------------------------------"
@@ -63,6 +51,8 @@ fn main() -> std::io::Result<()> {
         println!(
             "-----------------------------------------------------------------------------------------"
         );
+
+        println!("{:?}", dir.path());
     }
     Ok(())
 }
