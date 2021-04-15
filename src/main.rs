bac88718cf89e1e30f13e6cf08ae59ef2004e8cd
Kobby Pentangeli <kobbypentangeli@gmail.com>
2021-04-15 01:08:08 +0000

    feat: Implement functionality for reading multiple JSON files

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,61 @@
 use itertools::Itertools;
-use serde::{Deserialize, Serialize};
-use std::path::Path;
+use serde::Deserialize;
 use std::fs::File;
 use std::time::{Duration, SystemTime};
+use walkdir::WalkDir;
 
-#[derive(Clone, Debug, Deserialize, Serialize, PartialEq)]
-pub struct OraoStream {
-    pub provider_id: u32,
-    pub key: u64,
-    pub value: f64,
-    pub timestamp: u128,
+#[derive(Clone, Debug, Deserialize, PartialEq)]
+pub struct Message {
+    pub provider_id: i32,
+    pub key: i32,
+    pub value: f32,
+    pub timestamp: i64,
 }
 
 fn main() {
-    let json_file_path = Path::new("../data.json");
-    let file = File::open(json_file_path).expect("file not found");
-    let packets: Vec<OraoStream> = serde_json::from_reader(file).expect("error while reading file");
+    // Extract files
+    let mut files: Vec<File> = Vec::new();
+    for entry in WalkDir::new("../stream").into_iter().filter_map(|e | e.ok()) {
+        let json_file_path = entry.path();
+        let file = File::open(json_file_path).expect("File not found");
+        files.push(file);
+    }
 
-    // remove duplicates
-    let uniques = packets
-        .clone()
-        .into_iter()
-        .dedup_by(|x, y| x.timestamp - y.timestamp <= 100)
-        .collect::<Vec<OraoStream>>();
+    // Map files to `Message` structs
+    let mut packets: Vec<Vec<Message>> = Vec::new();
+    for file in files {
+        let data: Vec<Message> = serde_json::from_reader(file).expect("Error while reading");
+        packets.push(data);
+    }
 
-    let good_lines = uniques.len();
-    let maximum_gap = packets.len() - uniques.len();
-    let average_gap = packets.len() / maximum_gap;
+    for data in &packets {
+        // Remove duplicates
+        let uniques = data
+            .clone()
+            .into_iter()
+            .dedup_by(|x, y| x.timestamp - y.timestamp <= 100)
+            .collect::<Vec<Message>>();
 
-    println!("-----------------------------------------------------------------------------------");
-    println!(
-        "| Time elapsed: {:.2} | Amount of good lines: {} |  Maximum gap: {} |  Average gap: {} |",
-        SystemTime::now()
-            .duration_since(SystemTime::UNIX_EPOCH)
-            .unwrap_or(Duration::from_secs(0))
-            .as_secs_f64(),
-        good_lines,
-        maximum_gap,
-        average_gap
-    );
-    println!("-----------------------------------------------------------------------------------");
+        // Calculate metrics
+        let good_lines = uniques.len();
+        let maximum_gap = &packets.len() - uniques.len();
+        let average_gap = packets.len() / maximum_gap;
+
+        println!(
+            "-----------------------------------------------------------------------------------------"
+        );
+        println!(
+            "| Time elapsed: {:.2} | Amount of good lines: {} |  Maximum gap: {} |  Average gap: {} |",
+            SystemTime::now()
+                .duration_since(SystemTime::UNIX_EPOCH)
+                .unwrap_or(Duration::from_secs(0))
+                .as_secs_f64(),
+            good_lines,
+            maximum_gap,
+            average_gap
+        );
+        println!(
+            "-----------------------------------------------------------------------------------------"
+        );
+    }
 }
