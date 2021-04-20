33553cb20a50dfc28e3563e8437f288a9e872ee0
Kobby Pentangeli <kobbypentangeli@gmail.com>
2021-04-20 15:15:23 +0000

    perf: Improve calculation of data-quality metrics

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,6 @@ use serde::Deserialize;
 use std::fs;
 use std::time::{Duration, SystemTime};
 
-/// Structure of a block of JSON data
-#[derive(Clone, Debug, Deserialize, PartialEq)]
-pub struct Message {
-    pub provider_id: i32,
-    pub key: i32,
-    pub value: f32,
-    pub timestamp: i64,
-}
-
 fn main() -> std::io::Result<()> {
     for entry in fs::read_dir("./stream")? {
         let dir = entry?;
@@ -23,18 +14,11 @@ fn main() -> std::io::Result<()> {
         let file = fs::File::open(path).expect("File not found");
         let data: Vec<Message> = serde_json::from_reader(file).expect("Error while reading");
 
-        // Remove duplicates
-        let uniques = data
-            .clone()
-            .into_iter()
-            .dedup_by(|x, y| x.provider_id == y.provider_id && x.key == y.key)
-            .collect::<Vec<Message>>();
-
         // Calculate metrics
-        // TODO:__ 
+        let maximum_gap = calculate_max_gap(&data).unwrap();
+        let average_gap = calculate_avg_gap(&data);
+        let uniques = remove_duplicates(data);
         let good_lines = uniques.len();
-        let maximum_gap = data.len() - uniques.len();
-        let average_gap = data.len() / maximum_gap;
 
         println!(
             "-----------------------------------------------------------------------------------------"
@@ -56,4 +40,35 @@ fn main() -> std::io::Result<()> {
         println!("{:?}", dir.path());
     }
     Ok(())
+}
+
+/// Structure of a block of JSON data
+#[derive(Clone, Debug, Deserialize, PartialEq)]
+pub struct Message {
+    provider_id: i32,
+    key: i32,
+    value: f32,
+    timestamp: i64,
+}
+
+/// Removes duplicate lines with respect to same provider ID and key
+pub fn remove_duplicates(data: Vec<Message>) -> Vec<Message> {
+    data
+        .into_iter()
+        .dedup_by(|x, y| x.provider_id == y.provider_id && x.key == y.key)
+        .collect::<Vec<Message>>()
+}
+
+/// Computes the largest gap between two timestamps
+pub fn calculate_max_gap(data: &[Message]) -> Option<i64> {
+    data.windows(2)
+            .map(|message| (message[0].timestamp - message[1].timestamp).abs())
+            .max_by(|x, y| x.partial_cmp(y).unwrap())
+}
+
+/// Computes the average on all gaps
+pub fn calculate_avg_gap(data: &[Message]) -> i64 {
+    data.windows(2)
+        .map(|message| (message[0].timestamp - message[1].timestamp).abs())
+        .sum::<i64>()
 }
